9f7b5403784038246c732fa3820d71ef36d5b93e
jjtan <jjtan@jjtandeMacBook-Air.local>
2026-04-02 16:29:32 +0800

    fix: model path

--- scripts/infer/session_ranking.sh
+++ scripts/infer/session_ranking.sh
@@ -5,7 +5,7 @@ RUNTIME_ENV=${RUNTIME_ENV:-"./configs/runtime_env.yaml"}
 dataset_names=(locomo split_longmemeval_s personamem_128k personamem_1M zh4o personamem_v2_32k personamem_v2_128k split_longmemeval_m HotpotQA WebWalkerQA-Silver WebDancer)
 dataset_splits=(test)
 
-model_name="MemSifter/ep1-DAPO-Qwen3-4B-Task-Reward-step-80"
+model_name="zstanjj/MemSifter-4B-Thinking"
 
 max_num_batched_tokens=$((130*1024))
 max_model_len=$((146*1024))
